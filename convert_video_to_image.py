8befdeae61bdc3489167cc919af0485623232b71
Tien <viettienha98@gmail.com>
2020-07-27 14:42:03 +0700

    Change config train_times => 1

--- convert_video_to_image.py
+++ convert_video_to_image.py
@@ -1,11 +1,11 @@
 import cv2
 
 if __name__ == "__main__":
-    vidcap = cv2.VideoCapture('GR1-test.mp4')
+    vidcap = cv2.VideoCapture('GR-test-2.mp4')
     success, image = vidcap.read()
     count = 0
     while success:
-        if count % 100 == 0:
-            cv2.imwrite("test_data/frame%d.jpg" % count, image)
+        if count % 50 == 0:
+            cv2.imwrite("test_data_2/frame%d.jpg" % count, image)
         success, image = vidcap.read()
         count += 1
