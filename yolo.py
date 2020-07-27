9a7ccb7d074d5a65c87de0815529cb846fb6582d
Tien <viettienha98@gmail.com>
2020-07-27 15:56:17 +0700

    Change some config

--- yolo.py
+++ yolo.py
@@ -19,7 +19,8 @@ class YoloLayer(Layer):
         # make the model settings persistent
         self.ignore_thresh = ignore_thresh
         self.warmup_batches = warmup_batches
-        self.anchors = tf.constant(anchors, dtype='float', shape=[1, 1, 1, 3, 2])
+        self.anchors = tf.constant(
+            anchors, dtype='float', shape=[1, 1, 1, 3, 2])
         self.grid_scale = grid_scale
         self.obj_scale = obj_scale
         self.noobj_scale = noobj_scale
@@ -29,21 +30,24 @@ class YoloLayer(Layer):
         # make a persistent mesh grid
         max_grid_h, max_grid_w = max_grid
 
-        cell_x = tf.to_float(tf.reshape(tf.tile(tf.range(max_grid_w), [max_grid_h]),
-                                        (1, max_grid_h, max_grid_w, 1, 1)))
+        cell_x = tf.cast(tf.reshape(tf.tile(tf.range(max_grid_w), [max_grid_h]),
+                                    (1, max_grid_h, max_grid_w, 1, 1)), tf.float32)
         cell_y = tf.transpose(cell_x, (0, 2, 1, 3, 4))
-        self.cell_grid = tf.tile(tf.concat([cell_x, cell_y], -1), [batch_size, 1, 1, 3, 1])
+        self.cell_grid = tf.tile(
+            tf.concat([cell_x, cell_y], -1), [batch_size, 1, 1, 3, 1])
 
         super(YoloLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        super(YoloLayer, self).build(input_shape)  # Be sure to call this somewhere!
+        # Be sure to call this somewhere!
+        super(YoloLayer, self).build(input_shape)
 
     def call(self, x):
         input_image, y_pred, y_true, true_boxes = x
 
         # adjust the shape of the y_predict [batch, grid_h, grid_w, 3, 4+1+nb_class]
-        y_pred = tf.reshape(y_pred, tf.concat([tf.shape(y_pred)[:3], tf.constant([3, -1])], axis=0))
+        y_pred = tf.reshape(y_pred, tf.concat(
+            [tf.shape(y_pred)[:3], tf.constant([3, -1])], axis=0))
 
         # initialize the masks
         object_mask = tf.expand_dims(y_true[..., 4], 4)
@@ -54,17 +58,20 @@ class YoloLayer(Layer):
         # compute grid factor and net factor
         grid_h = tf.shape(y_true)[1]
         grid_w = tf.shape(y_true)[2]
-        grid_factor = tf.reshape(tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 2])
+        grid_factor = tf.reshape(
+            tf.cast([grid_w, grid_h], tf.float32), [1, 1, 1, 1, 2])
 
         net_h = tf.shape(input_image)[1]
         net_w = tf.shape(input_image)[2]
-        net_factor = tf.reshape(tf.cast([net_w, net_h], tf.float32), [1, 1, 1, 1, 2])
+        net_factor = tf.reshape(
+            tf.cast([net_w, net_h], tf.float32), [1, 1, 1, 1, 2])
 
         # Adjust prediction
         pred_box_xy = (self.cell_grid[:, :grid_h, :grid_w, :, :] + tf.sigmoid(
             y_pred[..., :2]))  # sigma(t_xy) + c_xy
         pred_box_wh = y_pred[..., 2:4]  # t_wh
-        pred_box_conf = tf.expand_dims(tf.sigmoid(y_pred[..., 4]), 4)  # adjust confidence
+        pred_box_conf = tf.expand_dims(tf.sigmoid(
+            y_pred[..., 4]), 4)  # adjust confidence
         pred_box_class = y_pred[..., 5:]  # adjust class probabilities
 
         # Adjust ground truth
@@ -86,7 +93,8 @@ class YoloLayer(Layer):
         true_maxes = true_xy + true_wh_half
 
         pred_xy = tf.expand_dims(pred_box_xy / grid_factor, 4)
-        pred_wh = tf.expand_dims(tf.exp(pred_box_wh) * self.anchors / net_factor, 4)
+        pred_wh = tf.expand_dims(
+            tf.exp(pred_box_wh) * self.anchors / net_factor, 4)
 
         pred_wh_half = pred_wh / 2.
         pred_mins = pred_xy - pred_wh_half
@@ -105,7 +113,8 @@ class YoloLayer(Layer):
         iou_scores = tf.truediv(intersect_areas, union_areas)
 
         best_ious = tf.reduce_max(iou_scores, axis=4)
-        conf_delta *= tf.expand_dims(tf.to_float(best_ious < self.ignore_thresh), 4)
+        conf_delta *= tf.expand_dims(tf.to_float(best_ious <
+                                                 self.ignore_thresh, tf.float32), 4)
 
         # Compute some online statistics
         # true_xy = true_box_xy / grid_factor
@@ -158,16 +167,18 @@ class YoloLayer(Layer):
         wh_scale = tf.expand_dims(2 - wh_scale[..., 0] * wh_scale[..., 1],
                                   axis=4)  # the smaller the box, the bigger the scale
 
-        xy_delta = xywh_mask * (pred_box_xy - true_box_xy) * wh_scale * self.xywh_scale
-        wh_delta = xywh_mask * (pred_box_wh - true_box_wh) * wh_scale * self.xywh_scale
+        xy_delta = xywh_mask * (pred_box_xy - true_box_xy) * \
+            wh_scale * self.xywh_scale
+        wh_delta = xywh_mask * (pred_box_wh - true_box_wh) * \
+            wh_scale * self.xywh_scale
         conf_delta = object_mask * (pred_box_conf - true_box_conf) * self.obj_scale + \
-                     (1 - object_mask) * conf_delta * self.noobj_scale
+            (1 - object_mask) * conf_delta * self.noobj_scale
         class_delta = object_mask * \
-                      tf.expand_dims(
-                          tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class,
-                                                                         logits=pred_box_class),
-                          4) * \
-                      self.class_scale
+            tf.expand_dims(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(labels=true_box_class,
+                                                               logits=pred_box_class),
+                4) * \
+            self.class_scale
 
         loss_xy = tf.reduce_sum(tf.square(xy_delta), list(range(1, 5)))
         loss_wh = tf.reduce_sum(tf.square(wh_delta), list(range(1, 5)))
@@ -214,7 +225,8 @@ def _conv_block(inp, convs, do_skip=True):
                    name='conv_' + str(conv['layer_idx']),
                    use_bias=not conv['bnorm'])(x)
         if conv['bnorm']:
-            x = BatchNormalization(epsilon=0.001, name='bnorm_' + str(conv['layer_idx']))(x)
+            x = BatchNormalization(
+                epsilon=0.001, name='bnorm_' + str(conv['layer_idx']))(x)
         if conv['leaky']:
             x = LeakyReLU(alpha=0.1, name='leaky_' + str(conv['layer_idx']))(x)
 
@@ -249,26 +261,34 @@ def create_yolov3_model(
 
     # Layer  0 => 4
     x = _conv_block(input_image, [
-        {'filter': 32, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 0},
-        {'filter': 64, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 1},
-        {'filter': 32, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 2},
+        {'filter': 32, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 0},
+        {'filter': 64, 'kernel': 3, 'stride': 2,
+            'bnorm': True, 'leaky': True, 'layer_idx': 1},
+        {'filter': 32, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 2},
         {'filter': 64, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 3}])
 
     # Layer  5 => 8
     x = _conv_block(x, [
-        {'filter': 128, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 5},
-        {'filter': 64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 6},
+        {'filter': 128, 'kernel': 3, 'stride': 2,
+            'bnorm': True, 'leaky': True, 'layer_idx': 5},
+        {'filter': 64, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 6},
         {'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 7}])
 
     # Layer  9 => 11
     x = _conv_block(x, [
-        {'filter': 64, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 9},
+        {'filter': 64, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 9},
         {'filter': 128, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 10}])
 
     # Layer 12 => 15
     x = _conv_block(x, [
-        {'filter': 256, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 12},
-        {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 13},
+        {'filter': 256, 'kernel': 3, 'stride': 2,
+            'bnorm': True, 'leaky': True, 'layer_idx': 12},
+        {'filter': 128, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 13},
         {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 14}])
 
     # Layer 16 => 36
@@ -282,8 +302,10 @@ def create_yolov3_model(
 
     # Layer 37 => 40
     x = _conv_block(x, [
-        {'filter': 512, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 37},
-        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 38},
+        {'filter': 512, 'kernel': 3, 'stride': 2,
+            'bnorm': True, 'leaky': True, 'layer_idx': 37},
+        {'filter': 256, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 38},
         {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 39}])
 
     # Layer 41 => 61
@@ -297,8 +319,10 @@ def create_yolov3_model(
 
     # Layer 62 => 65
     x = _conv_block(x, [
-        {'filter': 1024, 'kernel': 3, 'stride': 2, 'bnorm': True, 'leaky': True, 'layer_idx': 62},
-        {'filter': 512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 63},
+        {'filter': 1024, 'kernel': 3, 'stride': 2,
+            'bnorm': True, 'leaky': True, 'layer_idx': 62},
+        {'filter': 512, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 63},
         {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 64}])
 
     # Layer 66 => 74
@@ -310,16 +334,21 @@ def create_yolov3_model(
 
     # Layer 75 => 79
     x = _conv_block(x, [
-        {'filter': 512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 75},
-        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 76},
-        {'filter': 512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 77},
-        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 78},
+        {'filter': 512, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 75},
+        {'filter': 1024, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 76},
+        {'filter': 512, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 77},
+        {'filter': 1024, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 78},
         {'filter': 512, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 79}],
-                    do_skip=False)
+        do_skip=False)
 
     # Layer 80 => 82
     pred_yolo_1 = _conv_block(x, [
-        {'filter': 1024, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 80},
+        {'filter': 1024, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 80},
         {'filter': (3 * (5 + nb_class)), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
          'layer_idx': 81}], do_skip=False)
     loss_yolo_1 = YoloLayer(anchors[12:],
@@ -336,22 +365,27 @@ def create_yolov3_model(
     # Layer 83 => 86
     x = _conv_block(x, [
         {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 84}],
-                    do_skip=False)
+        do_skip=False)
     x = UpSampling2D(2)(x)
     x = concatenate([x, skip_61])
 
     # Layer 87 => 91
     x = _conv_block(x, [
-        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 87},
-        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 88},
-        {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 89},
-        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 90},
+        {'filter': 256, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 87},
+        {'filter': 512, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 88},
+        {'filter': 256, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 89},
+        {'filter': 512, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 90},
         {'filter': 256, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 91}],
-                    do_skip=False)
+        do_skip=False)
 
     # Layer 92 => 94
     pred_yolo_2 = _conv_block(x, [
-        {'filter': 512, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 92},
+        {'filter': 512, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 92},
         {'filter': (3 * (5 + nb_class)), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
          'layer_idx': 93}], do_skip=False)
     loss_yolo_2 = YoloLayer(anchors[6:12],
@@ -368,18 +402,24 @@ def create_yolov3_model(
     # Layer 95 => 98
     x = _conv_block(x, [
         {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 96}],
-                    do_skip=False)
+        do_skip=False)
     x = UpSampling2D(2)(x)
     x = concatenate([x, skip_36])
 
     # Layer 99 => 106
     pred_yolo_3 = _conv_block(x, [
-        {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 99},
-        {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 100},
-        {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 101},
-        {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 102},
-        {'filter': 128, 'kernel': 1, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 103},
-        {'filter': 256, 'kernel': 3, 'stride': 1, 'bnorm': True, 'leaky': True, 'layer_idx': 104},
+        {'filter': 128, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 99},
+        {'filter': 256, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 100},
+        {'filter': 128, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 101},
+        {'filter': 256, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 102},
+        {'filter': 128, 'kernel': 1, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 103},
+        {'filter': 256, 'kernel': 3, 'stride': 1,
+            'bnorm': True, 'leaky': True, 'layer_idx': 104},
         {'filter': (3 * (5 + nb_class)), 'kernel': 1, 'stride': 1, 'bnorm': False, 'leaky': False,
          'layer_idx': 105}], do_skip=False)
     loss_yolo_3 = YoloLayer(anchors[:6],
