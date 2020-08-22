4d114f140e07a73f0043142aa7153bb2679791c5
Tien <viettienha98@gmail.com>
2020-08-22 09:49:51 +0700

    remove extract by path

--- server/search_engine/extractor.py
+++ server/search_engine/extractor.py
@@ -1,14 +1,15 @@
 import sys
 import os
-from server.search_engine.model import resnet50
+from search_engine.model import resnet50
 from torchvision import transforms
 import torch
 import PIL
 import functools
 import numpy as np
+from db import *
 
 
-class Extractor():
+class Extractor(metaclass=Singleton):
     def __init__(self, size=248):
         self.model = resnet50(pretrained=True, progress=True)
         self.size = size
@@ -34,27 +35,6 @@ class Extractor():
 
         return self.model.extract(images_tensor).detach().cpu().numpy()
 
-    def extract_database(self, data_path, n=4):
-        """
-            n: chunk size
-            Output: {cls: [], ...}
-        """
-        # Return dict
-        result = {}
-        for cls in os.listdir(data_path):
-            cls_path = os.path.join(data_path, cls)
-            image_list = [PIL.Image.open(os.path.join(cls_path, ip))
-                          for ip in os.listdir(cls_path)]
-            image_chunk = [image_list[i:i+n]
-                           for i in range(0, len(image_list), n)]
-            # print(image_chunk)
-            cls_feature = []
-            for image in image_chunk:
-                cls_feature.append(self.extract_many(image))
-            cls_feature = np.concatenate(cls_feature)
-            result[cls] = cls_feature
-        return result
-
     def preprocess(self, image):
         return torch.unsqueeze(self.tfms(image), 0)
 
@@ -64,8 +44,3 @@ class Extractor():
 
 if __name__ == "__main__":
     e = Extractor()
-    e.extract_database('server/image_data', n=2)
-    # images = PIL.Image.open("/home/tienhv/Pictures/fire.jpg")
-
-    # feature = e.extract(images)
-    # print(feature.shape)
