b217ed5d2f25c12909b26ed694c8347c02e7042e
Tien <viettienha98@gmail.com>
2020-11-07 09:28:16 +0700

    Frontend for app

---
+++ frontend/src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+import './App.style.css';
+
+const ENDPOINT = "http://0.0.0.0:5001"
+const axios = require('axios');
+
+const App = () => {
+  const [image, setImage] = useState("");
+  const [logs, setLogs] = useState([]);
+  const [logCounter, setLogCounter] = useState(0);
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('image', data => {
+      let byteArray = data.image;
+      let img = 'data:image/png;base64,' + byteArray
+      setImage(img);
+    });
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+  }, [])
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on('log', data => {
+      let new_logs = [...logs];
+      console.log(new_logs);
+      new_logs.push({ 'id': logCounter, 'log': data.log });
+      let a = logCounter + 1;
+      setLogCounter(a);
+      setLogs(new_logs);
+    });
+
+    // CLEAN UP THE EFFECT
+    return () => socket.disconnect();
+  }, [logCounter, logs]);
+
+  return (
+    <div>
+      <h1 className="title">Product watcher</h1>
+      <div className="main" >
+        <div className="left">
+          <img src={image} className="image" alt="Img" />
+        </div>
+        <div className="right">
+          {logs.map((log) => <p className="log" key={log.id}>{log.log}</p>)}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
