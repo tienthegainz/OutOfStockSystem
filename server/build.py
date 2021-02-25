f7ec984ac47682b3f49fd496eba5a5ad0e7b91e3
Tien <viettienha98@gmail.com>
2021-02-25 10:15:53 +0700

    try,catch build

--- server/build.py
+++ server/build.py
@@ -4,12 +4,19 @@ import os
 
 if __name__ == '__main__':
     from model import *
-    db.drop_all()
-    db.create_all()
-    camera = Camera(name='Raspberry Pi 3')
-    db.session.add(camera)
-    db.session.commit()
-    print(camera.id)
-    ann_path = os.path.join(STORAGE['path'], STORAGE['ann'], 'index.bin')
-    os.remove(ann_path)
-    print('Removed ', ann_path)
+    try:
+        db.drop_all()
+        db.create_all()
+        camera = Camera(name='Raspberry Pi 3')
+        db.session.add(camera)
+        db.session.commit()
+        print(camera.id)
+    except Exception as e:
+        print(e)
+
+    try:
+        ann_path = os.path.join(STORAGE['path'], STORAGE['ann'], 'index.bin')
+        os.remove(ann_path)
+        print('Removed ', ann_path)
+    except Exception as e:
+        print(e)
