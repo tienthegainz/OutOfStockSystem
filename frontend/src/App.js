be34dad0764d7891ef80df68eab07741e5fbf3ce
Tien <viettienha98@gmail.com>
2021-04-09 16:02:11 +0700

    Add notification on browser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import { io } from "socket.io-client";
 import ProductWatcherPage from './pages/product_watcher/ProductWatcher';
 import CameraPage from './pages/camera/Camera';
 import NavBar from "./components/NavBar/NavBar";
@@ -15,11 +16,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import UserPage from "./pages/employee/Employee";
 import { notification } from 'antd';
 import allActions from "./actions";
+import Notification from "react-web-notification";
+
+const ENDPOINT = "http://0.0.0.0:5001"
 
 const App = () => {
   const userInfo = useSelector(state => state.currentUser);
   const notiInfo = useSelector(state => state.notification);
   const dispatch = useDispatch();
+  const [notiMessage, setNotiMessage] = useState({
+    title: "",
+    message: "",
+    display: false
+  });
 
   const openNotification = (title, message) => {
     notification.error({
@@ -35,9 +44,42 @@ const App = () => {
       openNotification(notiInfo.title, notiInfo.message);
       dispatch(allActions.notiActions.cancel());
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [notiInfo]);
 
+  useEffect(() => {
+    const camera_socket = io(ENDPOINT);
+    camera_socket.on('noti', data => {
+      console.log({ ...data, display: true });
+      setNotiMessage({ ...data, display: true });
+    });
+
+    // CLEAN UP THE EFFECT
+    if (camera_socket)
+      return () => camera_socket.disconnect();
+  }, []);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setNotiMessage({
+  //       title: "",
+  //       message: "",
+  //       display: false
+  //     })
+  //   }, 3000);
+  // }, [notiMessage]);
+
   return (<React.Fragment>
+    {notiMessage.display ? <Notification
+      ignore={false}
+      timeout={3000}
+      title={notiMessage.title}
+      options={
+        {
+          body: notiMessage.message
+        }
+      }
+    /> : null}
     {userInfo.isLoggedIn ? <Router>
       <NavBar />
       <Switch>
